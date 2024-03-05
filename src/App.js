1007df21b30a5cc39280a62ba19d07bfd8d04165
Rafaqat Ali <rafaqatmehar007@gmail.com>
2024-03-05 13:01:34 +0500

    inatial

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Hello react
+        Hello learning react
         </a>
       </header>
     </div>
